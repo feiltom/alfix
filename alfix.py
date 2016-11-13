feiltom/alfix

commit 8c7f780c4f952bf6969b29aebe8a7e289dcf0256

Render SVG images too
The SVG images are compressed but do not indicate that by file extension,
so mime type guessing by file extension is no good, so do_GET() from
SimpleHTTPServer is not enough.  The SVG images also contain some Adobe
Illustrator specific gunk which at least Firefox refuses to handle at all,
so we need to massage that a bit while serving it.

Luckily all other content seems to be nicely typed so we can just
assume octet-stream to mean compressed SVG. Use gzip module to read,
and parse with entity resolving off to filter out the bad apples,
and feed the result to the now happy browser.

Refactor do_page() to handle different content types and take advantage
of that in all serving of our own.

alfix.py
@@ -5,6 +5,7 @@ import sqlite3
 import http.server
 import socketserver
 import socket
+import gzip
 from lxml import etree
 from lxml import html
 from lxml.html import builder as E
@@ -185,12 +186,9 @@ class myHandler(http.server.SimpleHTTPRequestHandler):
 
         self.do_elements('contents', values, what)
 
-    def do_page(self):
-        out = html.tostring(self.page,
-                            pretty_print=True,
-                            doctype='<!DOCTYPE html>')
+    def do_page(self, ctype, out):
         self.send_response(HTTPStatus.OK)
-        self.send_header('Content-type', 'text/html; charset=utf-8')
+        self.send_header('Content-type', ctype)
         self.send_header('Content-Length', len(out))
         self.end_headers()
 
@@ -246,17 +244,37 @@ class myHandler(http.server.SimpleHTTPRequestHandler):
                 self.do_path()
 
 
-        self.do_page()
+        out = html.tostring(self.page,
+                            pretty_print=True,
+                            doctype='<!DOCTYPE html>')
+        self.do_page('text/html; charset=utf-8', out)
+
+    def do_svg(self):
+        f = gzip.open(self.path)
+        # The SVG files specify an undefined &ns_graphs entity on which
+        # at least Firefox barfs on. Parsing the SVG file with entity
+        # resolving disabled strips off this anomaly.
+        parser = etree.XMLParser(resolve_entities=False)
+        svg = etree.parse(f, parser=parser)
+        out = etree.tostring(svg)
+        f.close()
+
+        self.do_page('image/svg+xml', out)
 
     def do_GET(self):
         self.p = urlparse(self.path)
+
 
         if self.p.path in [ '/' ]:
             self.q = dict(parse_qsl(self.p.query))
             return self.do_database()
         else:
             self.path = web_base + self.path
-            return http.server.SimpleHTTPRequestHandler.do_GET(self)
+            # SVG files are compressed and need some other love too
+            if self.guess_type(self.path) == 'application/octet-stream':
+                return self.do_svg()
+            else:
+                return http.server.SimpleHTTPRequestHandler.do_GET(self)
 
 if __name__ == '__main__':
     HOST = 'localhost'

commit dc8d9e53ee02b6b101cbf869f4eb21d1d299ab99

Unbreak non-SVG image rendering. Oops.
Commit 8c7f780c4f952bf6969b29aebe8a7e289dcf0256 optimistically stated
that all other content gets correctly typed. Well, the arrows and
other decorative stuff from images/ directory is because they got
normal suffixes, the ones we care about (from image/ directory) do
not. So we need to handle both image types manually.

Unbreaks non-SVG image rendering broken in commit
8c7f780c4f952bf6969b29aebe8a7e289dcf0256.

alfix.py
@@ -300,17 +300,24 @@ class myHandler(http.server.SimpleHTTPRequestHandler):
                             doctype='<!DOCTYPE html>')
         self.do_page('text/html; charset=utf-8', out)
 
-    def do_svg(self):
-        f = gzip.open(self.path)
-        # The SVG files specify an undefined &ns_graphs entity on which
-        # at least Firefox barfs on. Parsing the SVG file with entity
-        # resolving disabled strips off this anomaly.
-        parser = etree.XMLParser(resolve_entities=False)
-        svg = etree.parse(f, parser=parser)
-        out = etree.tostring(svg)
-        f.close()
+    def do_image(self):
+        try:
+            f = gzip.open(self.path)
+            # The SVG files specify an undefined &ns_graphs entity on which
+            # at least Firefox barfs on. Parsing the SVG file with entity
+            # resolving disabled strips off this anomaly.
+            parser = etree.XMLParser(resolve_entities=False)
+            svg = etree.parse(f, parser=parser)
+            out = etree.tostring(svg)
+            t = 'image/svg+xml'
+        except OSError:
+            f = open(self.path, 'rb')
+            out = f.read()
+            t = 'image/jpeg'
+        finally:
+            f.close()
 
-        self.do_page('image/svg+xml', out)
+        self.do_page(t, out)
 
     def do_GET(self):
         self.p = urlparse(self.path)
@@ -323,7 +330,7 @@ class myHandler(http.server.SimpleHTTPRequestHandler):
             self.path = web_base + self.path
             # SVG files are compressed and need some other love too
             if self.guess_type(self.path) == 'application/octet-stream':
-                return self.do_svg()
+                return self.do_image()
             else:
                 return http.server.SimpleHTTPRequestHandler.do_GET(self)
 